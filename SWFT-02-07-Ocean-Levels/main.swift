fec809bf7141d838720d63b5663e27227564be13
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 05:31:09 -0400

    Initial Commit

---
+++ SWFT-02-07-Ocean-Levels/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-02-07-Ocean-Levels
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
